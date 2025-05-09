3379d1a1e9fab4a4423cff55d3cb75a3be2a04ce
Víctor Pérez <vmpch1@outlook.com>
2025-05-09 00:08:18 -0600

    feat: create initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="calculator">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+		<div className="display">
+			<p className="operation">10 + 20</p>
+			<p className="current-number">2780</p>
+		</div>
+		<div className="buttons">
+			<button type="button" className="btn-clear">AC</button>
+			<button type="button" className="btn-del">DEL</button>
+			<button type="button" className="btn-x big-font">x</button>
+			<button type="button" className="btn-num">7</button>
+			<button type="button" className="btn-num">8</button>
+			<button type="button" className="btn-num">9</button>
+			<button type="button" className="btn-plus big-font">+</button>
+			<button type="button" className="btn-num">4</button>
+			<button type="button" className="btn-num">5</button>
+			<button type="button" className="btn-num">6</button>
+			<button type="button" className="btn-minus big-font">-</button>
+			<button type="button" className="btn-num">1</button>
+			<button type="button" className="btn-num">2</button>
+			<button type="button" className="btn-num">3</button>
+			<button type="button" className="btn-0">0</button>
+			<button type="button" className="btn-eq big-font">=</button>
+		</div>
       </div>
     </>
   )
